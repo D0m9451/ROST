769b6e86cac9dde688b1c6562b21217b04877890
Dom :) <dominic.j.milton@gmail.com>
2025-05-15 22:22:58 +0100

    fuckk whats going on

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,11 +1,32 @@
 fn main() {
     println!("Hello, world yegggo!");
+    count();
+}
 
-    let mut x = 0;
-    let y = 99999;
+fn count() {
+    let mut x: f64 = 0.0;
+    let y: f64 = 99.0;
 
     while x < y {
-        x += 1;
+        x += 1.0;
         println!("{}", x);
+    }
+    ifelse(x, y);
+}
+
+fn ifelse(x: f64, y: f64) {
+    let mut count = 0;
+    while true {
+        if x < 0.0 {
+            let mut x = y*x;
+            println!("{}", x);
+        }
+        else {
+            let mut x = 3.185*(y*x);
+        }
+        count += 1;
+        if x < 0.0 {
+            break;
+        }
     }
 }
