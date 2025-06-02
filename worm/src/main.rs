5e1008298986d2331e22c758b881f568bf40bca7
Dom :) <dominic.j.milton@gmail.com>
2025-06-02 10:28:41 +0100

    sids ip

--- worm/src/main.rs
+++ worm/src/main.rs
@@ -2,8 +2,8 @@ use std::net::TcpStream;
 use std::io::Write;
 
 fn worm(depth: u32) {
-    if let Ok(mut stream) = TcpStream::connect("10.4.102.50:8080") {
-        stream.write(b"this message was sent via tcp").unwrap();
+    if let Ok(mut stream) = TcpStream::connect("10.5.8.192:8080") {
+        stream.write(b"this message was sent via tcp port: 8080").unwrap();
     }
 }
 
