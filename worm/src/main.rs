215cf4a8ae079ef9e5bd7e81ddfbf21b71945efd
Dom :) <dominic.j.milton@gmail.com>
2025-06-02 10:55:20 +0100

    no owrk

--- worm/src/main.rs
+++ worm/src/main.rs
@@ -1,15 +1,20 @@
-use std::net::TcpStream;
+use std::net::{UdpSocket, TcpListener, IpAddr, SocketAddr};
 use std::io::Write;
 
-fn worm(depth: u32) {
-    if let Ok(mut stream) = TcpStream::connect("10.5.8.192:8080") {
+fn send(depth: u32, addr:IpAddr) {
+    if let Ok(mut stream) = TcpStream::connect(addr) {
         stream.write(b"this message was sent via tcp port: 8080").unwrap();
     }
 }
 
+fn search(depth: u32) {
+    let target = "10.5.8.183";
+    let addr = SocketAddr::new(target:IpAddr, 8080);
+    send(depth: u32, addr)
 
+}
 
 
 fn main() {
-    worm(1);
+    search(1);
 }
