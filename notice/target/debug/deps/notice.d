c7ef8f85c068ba95654e952fdd42b434d66d3247
Dom :) <dominic.j.milton@gmail.com>
2025-06-26 14:44:49 +0100

    NOTICE!

---
+++ notice/target/debug/deps/notice.d
@@ -0,0 +1,5 @@
+C:\Users\Domin\Downloads\ComputerScience\Programs\ROST\notice\target\debug\deps\notice.exe: src\main.rs
+
+C:\Users\Domin\Downloads\ComputerScience\Programs\ROST\notice\target\debug\deps\notice.d: src\main.rs
+
+src\main.rs:
